9085b7c10683be21709845ee2eead9a9f98628cd
kimchj <wnmh123@naver.com>
2022-01-27 23:44:43 +0900

    modify Product item to component

--- src/data.js
+++ src/data.js
@@ -1,22 +1,30 @@
 
+// img
+import can from "./can.jpg";
+import miniCoke from "./miniCoke.jpg";
+import zeroCan from "./zeroCan.jpg";
+
 const data = () => [
     {
         id : 0,
         title : "One Coke Can",
         content : "just one coke can",
-        price : 1200
+        price : 1200,
+        image : {can}
     },
     {
         id : 1,
         title : "mini coke pets",
         content : "a lot of coke",
-        price : 32000
+        price : 32000,
+        image : {miniCoke}
     },
     {
         id : 2,
         title : "zero coke can",
         content : "zero calories coke can",
-        price : 1600
+        price : 1600,
+        image : {zeroCan}
     }
 ];
 
